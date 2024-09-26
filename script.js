b34754d919d0db825f517cbb2ce738388551c446
ayush2080 <140793902+ayush2080@users.noreply.github.com>
2024-09-26 18:24:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ document.getElementById('menuToggle').addEventListener('click', function() {
             navbarContent.classList.remove('show');
             document.getElementById('menuToggle').classList.remove('hide');
         });
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
