4c8475e4c1c6b912b7cc84a072096b992e72ae5d
ayush2080 <140793902+ayush2080@users.noreply.github.com>
2024-10-30 20:40:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -31,6 +31,13 @@ function topFunction() {
 }
 
 
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+  
+
+})
+
 
 
 
